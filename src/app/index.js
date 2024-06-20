cfdfd0986b053943ff19eb22de6cbb52b1df88f6
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2024-06-21 00:47:05 +0300

    Some webpack setting up

---
+++ src/app/index.js
@@ -0,0 +1,2 @@
+import "../assets/styles/style.scss";
+console.log("Hello World from main file!");
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: "localhost",
+      port: 3000,
+      proxy: "http://localhost:8080/",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+};
